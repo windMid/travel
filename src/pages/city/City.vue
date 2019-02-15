a41d2c2bba03a04eb760f87fec7108da76f5170d
weibo <745404604@qq.com>
2019-02-15 18:59:10 +0800

    city search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
 		<CityHeader></CityHeader>
+		<CitySearch></CitySearch>
 		<router-view/>
 	</div>
 
@@ -8,11 +9,13 @@
 
 <script>
 import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
 
 export default {
 	name: "City",
 	components: {
-		CityHeader
+		CityHeader,
+		CitySearch
 	}
 }
 </script>
